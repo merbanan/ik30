a898d0fbf4224b8a88c71a14d2edf831751b8c72
Benjamin Larsson <banan@ludd.ltu.se>
2019-05-29 23:27:39 +0200

    Get json formatted output

--- connection.py
+++ connection.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
+#run with python -u connection.py 192.168.1.2 9226 | mosquitto_pub -t Energy -l
+
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import re
 import time
 import telnetlib
-HOST ="192.168.100.102"
+import csv
+import sys
+
 PRECMD = "stream:42:val?com=gmp&mid="
 POSTCMD= ".1&regt=010a00\n"
 
 def connect_telnet(host):
     tn=telnetlib.Telnet(host,"7777")
     tn.read_until('stream:7:granted')
-    print 'Connected to IK30'
+#    print 'Connected to IK30'
     return tn
 
 def send_neuros_id(tn, nid):
@@ -56,23 +60,32 @@ def parse_ik30_resp(tn):
 #        print ("val: %s" % val.firstChild.data)
         return int(val.firstChild.data)
 
-
-
-tn = connect_telnet(HOST)
-send_neuros_id(tn, "05041d475900")
-
-# parse first response
-ret = parse_ik30_resp(tn)
-if ret==0:
-    print "Query ok %d\n" % ret
-
-if ret==0:
-    # parse second response
+def get_kWh_answer(tn):
+    # parse first response
     ret = parse_ik30_resp(tn)
-    if ret == -2:
-        print "Query error %d\n" % ret
-    if ret > 0:
-        print "Current kWh %d\n" % ret
-        
+#    if ret==0:
+#        print "Query ok %d\n" % ret
+
+    if ret==0:
+        # parse second response
+        ret = parse_ik30_resp(tn)
+        if ret == -2:
+#            print "Query error %d\n" % ret
+            return ret
+        if ret > 0:
+#            print "Current kWh %d\n" % ret
+            return ret
+
+
+tn = connect_telnet(sys.argv[1])
+
+with open(sys.argv[2]+'.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        send_neuros_id(tn, row['neurosid'])
+        kWh = get_kWh_answer(tn)
+        print "{ \"id\": %s, \"net\": %s, \"kWh\": %d }" % (row['id'], sys.argv[2], kWh)
+
+
 
 
